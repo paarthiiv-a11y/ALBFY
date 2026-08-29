bc9e79526640db0f32c653a802de65b3a12377c3
paarthiiv-a11y <paarthiiv@icloud.com>
2026-08-29 14:23:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 # ── telegram credentials ─────────────────────────────────────────────────────
-API_ID    = int(os.getenv("API_ID", "0"))
-API_HASH  = os.getenv("API_HASH", "")
+API_ID    = int(os.getenv("API_ID", "11983645"))
+API_HASH  = os.getenv("API_HASH", "67645172751678bec31dc03a1548cbe5")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
 # ── developer info ───────────────────────────────────────────────────────────
@@ -14,5 +14,5 @@ DEV_URL = os.getenv("DEV_URL", "https://t.me/DmOwner")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "0"))
 
 # ── mongodb ──────────────────────────────────────────────────────────────────
-MONGO_URI = os.getenv("MONGO_URI", "")
-DB_NAME   = os.getenv("DB_NAME", "spoti_music_bot")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://videshi:videshi@videshi.wtffv.mongodb.net/?appName=videshi")
+DB_NAME   = os.getenv("DB_NAME", "videshi")
